caa068851b56ad90fa339e6c0153addf1614951d
SJs <sumit.ai.dev@gmail.com>
2023-10-15 18:38:36 +0530

    height of a tree

---
+++ 9_Height_binaryTree.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int x){
+        data=x;
+        left=NULL;
+        right=NULL;
+    }
+};
+int calculateHeight(node* root)
+{
+    if (root==NULL)
+    {
+        return 0;
+    }
+    int lHeight=calculateHeight(root->left);
+    int rHeight=calculateHeight(root->right);
+    return max(lHeight,rHeight)+1;
+    
+}
+
+
+int main()
+{
+    node* root= new node(1);
+    root->left= new node(2);
+    root->right= new node(3);
+    root->left->left= new node(4);
+    root->left->right= new node(5);
+    root->right->left= new node(6);
+    root->right->right= new node(7);
+    cout<<calculateHeight(root);
+    
+    
+    return 0;
+}
